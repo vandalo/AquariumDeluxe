73f30c852e84dbc84566f1be3d55ae3a31839815
Dani <dani_rl94@hotmail.com>
2016-01-14 23:47:22 +0100

    assets

---
+++ core/src/com/mygdx/game/LogoScreen.java
@@ -0,0 +1,103 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.*;
+import static com.badlogic.gdx.math.Interpolation.*;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+public class LogoScreen implements Screen {
+
+	private OrthographicCamera camera;
+	final AquariumDeluxe game;
+	private Stage stage;
+	private Image image;
+	private Texture texture;
+	
+    public LogoScreen(final AquariumDeluxe gam) {
+        game = gam;
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 600, 480);
+    }
+    
+	@Override
+	public void show() {
+		stage = new Stage();
+
+		texture = new Texture("icon_dos.png");
+		image = new Image(texture);
+		image.setOrigin(image.getWidth()/2, image.getHeight()/2);
+		image.setPosition(Gdx.graphics.getWidth()/2- image.getWidth()/2, Gdx.graphics.getHeight()/2 - image.getHeight()/2);
+		image.setScale(0.4f);
+		image.addAction(fadeOut(0));
+		image.addAction(sequence(parallel(rotateBy(1080, 2f, swing), fadeIn(1.5f), scaleTo(2f, 2f, 2f)), delay(0.3f),
+				parallel(rotateBy(-720, 1f, Interpolation.pow2), scaleTo(0.1f, 0.1f, 1f)), scaleTo(1, 1), delay(0.5f),
+				run(new Runnable() {
+				    public void run () {
+				    	game.setScreen(new MainMenuScreen(game));
+			            dispose();
+				    }
+				})));
+		stage.addActor(image);		
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		camera.update();
+        stage.act(delta);
+        stage.draw();
+        if(Gdx.input.isTouched()){
+        	game.setScreen(new MainMenuScreen(game));
+            dispose();
+        }
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void hide() {
+	}
+
+	@Override
+	public void dispose() {
+		stage.dispose();
+		texture.dispose();
+	}
+
+}
+
+/*private ScheduledExecutorService programador;
+programador = Executors.newSingleThreadScheduledExecutor();
+programador.schedule(accion, timepohastalaprimeraejecucion, TimeUnit.MILLISECONDS);
+programador.schedule(accion, 2000, TimeUnit.MILLISECONDS);
+ private Runnable accion = new Runnable() {
+        @Override
+        public void run() {}};
+        ---------
+        *Timer timer = new Timer();
+        *TimerTask tas = new TimerTask(){
+	        public void run(){
+	        }
+	    };
+	    
+	 	timer.schedule(tas, 2900);*/
---
+++ core/src/com/mygdx/game/MainMenuScreen.java
@@ -0,0 +1,154 @@
+package com.mygdx.game;
+
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+public class MainMenuScreen implements Screen {
+
+	final AquariumDeluxe game;
+	final MainMenuScreen mmScreen;
+	private TextureAtlas atlas;
+	protected Skin skin;
+	private Stage stage;
+	private Table table;
+	private Label heading;
+	private TextButton buttonPlay, buttonExit;
+	protected Sprite background;
+	//private List<String> list;
+	//private ScrollPane scrollPane;
+	
+    public MainMenuScreen(final AquariumDeluxe gam) {
+        game = gam;
+        mmScreen = this;
+    }
+    
+	@Override
+	public void show() {
+		stage = new Stage(new FitViewport(800, 480)); //CON STRETCH Y BACKGROUND EN LA TABLA SE VE BN
+		Gdx.input.setInputProcessor(stage);
+		//((OrthographicCamera)stage.getCamera()).setToOrtho(false, 800, 480);
+		atlas = new TextureAtlas("skins/userInterface.pack");
+		skin = new Skin(Gdx.files.internal("skins/userInterface.json"), atlas);
+		table = new Table(skin);
+		table.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		
+		buttonPlay = new TextButton("PLAY", skin, "mainMenuBlack");
+		buttonPlay.pad(20);
+		buttonPlay.addListener(new ClickListener(){
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				//game.setScreen(new testGame(game));
+	            //dispose();
+				//game.setScreen(new StageSelector(mmScreen));
+			}
+		});
+		
+		buttonExit = new TextButton("EXIT", skin, "mainMenuBlack");
+		buttonExit.pad(20);
+		buttonExit.addListener(new ClickListener(){
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				dispose();
+	            Gdx.app.exit();
+			}
+		});
+		
+		heading = new Label("Tower Conquest", skin, "default");
+		//heading.setFontScale(2);
+		
+		table.add(heading);
+		table.getCell(heading).padBottom(50);
+		//table.setBackground(new Image(new Texture("globe.png")).getDrawable());
+		table.row();
+		table.add(buttonPlay);
+		table.getCell(buttonPlay).spaceBottom(15);
+		table.row();
+		table.add(buttonExit);
+		//table.debug();
+		table.setOrigin(table.getWidth()/2, table.getHeight()/2);
+		table.setFillParent(true);
+		stage.addActor(table);
+		
+		background = new Sprite(new Texture("background.png"));
+		background.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		
+		/*list = new List<String>(skin);
+		list.setItems(new String[]{"Uno", "dos", "tres"});
+		scrollPane = new ScrollPane(list, skin);
+		table.add("Select Level");
+		table.add(scrollPane);*/
+
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		game.batch.begin();
+			background.draw(game.batch);
+		game.batch.end();
+        //table.debugTable();
+		stage.getViewport().apply();
+        stage.act(delta);
+        stage.draw();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		stage.getViewport().update(width, height);
+		//table.invalidateHierarchy();
+		stage.getCamera().update();
+		//background.setBounds(0, 0, stage.getViewport().getWorldWidth(), stage.getViewport().getWorldHeight());
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void hide() {
+	}
+
+	@Override
+	public void dispose() {
+		atlas.dispose();
+		skin.dispose();
+		stage.dispose();
+		background.getTexture().dispose();
+	}
+
+}
+
+/*private ScheduledExecutorService programador;
+programador = Executors.newSingleThreadScheduledExecutor();
+programador.schedule(accion, timepohastalaprimeraejecucion, TimeUnit.MILLISECONDS);
+programador.schedule(accion, 2000, TimeUnit.MILLISECONDS);
+ private Runnable accion = new Runnable() {
+        @Override
+        public void run() {}};
+        ---------
+        *Timer timer = new Timer();
+        *TimerTask tas = new TimerTask(){
+	        public void run(){
+	        }
+	    };
+	    
+	 	timer.schedule(tas, 2900);        */
